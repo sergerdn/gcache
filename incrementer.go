761427fc892b0ac464fa9e5550e4eba113a0f008
sergerdn <64213648+sergerdn@users.noreply.github.com>
2021-01-23 06:43:17 +0300

    improve tests

--- incrementer.go
+++ incrementer.go
@@ -2,10 +2,10 @@ package gcache
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-type BaseLRUIncrementer interface {
+type BaseIncrementer interface {
 	Increment(k string, n int64) (interface{}, error)
 }
 
@@ -43,11 +43,12 @@ func incrementValue(v interface{}, n int64) (interface{}, error) {
 	return v, nil
 }
 
-// make sure that LRUIncrementer implements BaseLRUIncrementer
-var _ BaseLRUIncrementer = &LRUIncrementer{}
+// make sure that LRUIncrementer implements BaseIncrementer
+var _ BaseIncrementer = &LRUIncrementer{}
 
 type LRUIncrementer struct {
 	cache *LRUCache
+	lock  sync.RWMutex
 }
 
 func newLRUIncrementer(c *LRUCache) *LRUIncrementer {
@@ -55,34 +56,19 @@ func newLRUIncrementer(c *LRUCache) *LRUIncrementer {
 	return i
 }
 
-// Increment an item of type int, int8, int16, int32, int64, uintptr, uint,
-// uint8, uint32, or uint64, float32 or float64 by n. Returns an error if the
-// item's value is not an integer, if it was not found, or if it is not
-// possible to incrementValue it by n.
+// TODO: change to core implementation and remove custom lock
 func (i *LRUIncrementer) Increment(k string, n int64) (interface{}, error) {
-	i.cache.mu.Lock()
-	item, found := i.cache.items[k]
-	if !found {
-		i.cache.mu.Unlock()
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	v, err := i.cache.Get(k)
+	if err != nil {
+		i.lock.Unlock()
 		return nil, fmt.Errorf("item %s not found", k)
 	}
-
-	v := item.Value
-	it := v.(*lruItem)
-	now := time.Now()
-	if it.IsExpired(&now) {
-		i.cache.mu.Unlock()
-		return nil, fmt.Errorf("item %s not found", k)
-	}
-
-	vNew, err := incrementValue(it.value, n)
+	vNew, err := incrementValue(v, n)
 
 	if err != nil {
-		i.cache.mu.Unlock()
 		return nil, err
 	}
-
-	i.cache.items[k].Value = vNew
-	i.cache.mu.Unlock()
-	return vNew, nil
+	return vNew, i.cache.Set(k, vNew)
 }
